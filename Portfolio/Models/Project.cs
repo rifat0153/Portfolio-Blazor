60099e34061f9e33c51808bbb4244a12d3228f54
Mahbubur Rahman <rifat0153@gmail.com>
2023-08-21 20:18:24 +0100

    Add project files.

---
+++ Portfolio/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Models;
+
+public class Project
+{
+    public required int ProjectId { get; set; }
+
+    public string? ProjectName { get; set; }
+
+    public string? ProjectDescription { get; set; }
+
+    public string? ProjectUrl { get; set; }
+
+    public ICollection<string> Technologies { get; set; } = new List<string>();
+}
